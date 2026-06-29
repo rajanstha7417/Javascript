1dbb49dd5a5425edfec75834f5fb79161227f458
Rajan Shrestha <139777927+rajanstha7417@users.noreply.github.com>
2026-06-29 23:26:23 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert(3+3) we are using this in node not on browser
+
+//data types
+let name="rajan"
+let age=18
+let isLoggedIn= true
+
+//number
+//bigint
+//string
+//boolean
+//null -> standalone value
+//undefined
+//symbol-> used in react to find uniqueness
+
+
+//objects
+
+console.log(typeof(undefined))
+console.log(typeof(null))
+
